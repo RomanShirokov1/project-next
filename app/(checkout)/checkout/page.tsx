4e7a19d4f4d4c728342d201b91d5f1fc0cec6c6f
RomanShirokov1 <r0man_sh@mail.ru>
2025-01-12 14:20:32 +0300

    stories + final

--- app/(checkout)/checkout/page.tsx
+++ app/(checkout)/checkout/page.tsx
@@ -14,6 +14,7 @@ import {
   Container,
   Title,
 } from '@/components/shared';
+
 import { createOrder } from '@/app/actions';
 import toast from 'react-hot-toast';
 import { useSession } from 'next-auth/react';
